06969795d65b5cd66b4600148c54dc90305b707d
pblanchard <pierre.blanchard.pibl@orange.fr>
2014-05-13 12:53:38 +0200

    ajout support mvc

--- elearning-web/src/main/java/fr/iut/csid/empower/elearning/web/util/Constants.java
+++ elearning-web/src/main/java/fr/iut/csid/empower/elearning/web/util/Constants.java
@@ -2,5 +2,5 @@ package fr.iut.csid.empower.elearning.web.util;
 
 public class Constants
 {
-	public static final String APPLICATION_NAME = "ThymeLeaf Demo";
+	public static final String APPLICATION_NAME = "E-Learning";
 }
